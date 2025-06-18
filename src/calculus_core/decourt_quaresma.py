5bdab45d001e55d039fc7c2eea499a24e400b809
Kaio Silva <98492492+kaiosilva-dataeng@users.noreply.github.com>
2025-06-17 21:49:09 -0300

    refactor: streamline calcular_Np and calcular_Nl functions, improve error handling and variable usage

--- src/calculus_core/decourt_quaresma.py
+++ src/calculus_core/decourt_quaresma.py
@@ -1,51 +1,7 @@
-import math
+from calculus_core.models import Estaca, PerfilSPT
 
 
-class Estaca:
-    def __init__(
-        self,
-        tipo,
-        processo_construcao,
-        formato,
-        secao_transversal,
-        comprimento,
-    ):
-        if formato not in ['circular', 'quadrada']:
-            raise ValueError(
-                'Formato de estaca deve ser "circular" ou "quadrada".'
-            )
-        self.tipo = tipo
-        self.processo_construcao = processo_construcao
-        self.formato = formato
-        self.secao_transversal = secao_transversal
-        self.comprimento = comprimento
-
-    def area_ponta(self):
-        if self.formato == 'circular':
-            raio = self.secao_transversal / 2
-            return math.pi * (raio**2)
-        elif self.formato == 'quadrada':
-            largura = self.secao_transversal
-            return largura**2
-
-    def perimetro(self):
-        if self.formato == 'circular':
-            raio = self.secao_transversal / 2
-            return 2 * math.pi * raio
-        elif self.formato == 'quadrada':
-            largura = self.secao_transversal
-            return 4 * largura
-
-    def area_lateral(self):
-        if self.formato == 'circular':
-            raio = self.secao_transversal / 2
-            return math.pi * raio * self.comprimento
-        elif self.formato == 'quadrada':
-            largura = self.secao_transversal
-            return largura * self.comprimento
-
-
-def calcular_Np(perfil_spt, cota_asentamento):
+def calcular_Np(perfil_spt: PerfilSPT, cota_asentamento: int):
     """
     Calcula o Np_SPT médio na cota especificada,
     considerando a média do metro acima e abaixo.
@@ -57,24 +13,22 @@ def calcular_Np(perfil_spt, cota_asentamento):
     Returns:
         float: N_SPT médio na cota especificada.
     """
-    index_cota_asentamento = cota_asentamento - 1  # Index baseado em 0
 
-    if index_cota_asentamento < 0 or index_cota_asentamento >= len(perfil_spt):
+    if cota_asentamento not in perfil_spt:
         raise ValueError('Cota asentamento inválida para o perfil SPT.')
 
-    Nspt_cota_asentamento = perfil_spt[index_cota_asentamento][0]
+    medida_cota_asentamento = perfil_spt.obter_medida(cota_asentamento)
 
-    if index_cota_asentamento - 1 < 0:
-        Nspt_cota_acima = perfil_spt[index_cota_asentamento][0]
-    elif index_cota_asentamento - 1 >= 0:
-        Nspt_cota_acima = perfil_spt[index_cota_asentamento - 1][0]
+    medida_cota_acima = perfil_spt.obter_medida(
+        cota_asentamento - 1, aprox=True
+    )
+    medida_cota_abaixo = perfil_spt.obter_medida(cota_asentamento + 1)
 
-    if index_cota_asentamento + 1 >= len(perfil_spt):
-        Nspt_cota_abaixo = 50
-    elif index_cota_asentamento + 1 < len(perfil_spt):
-        Nspt_cota_abaixo = perfil_spt[index_cota_asentamento + 1][0]
-
-    return (Nspt_cota_asentamento + Nspt_cota_acima + Nspt_cota_abaixo) / 3
+    return (
+        medida_cota_asentamento.N_SPT
+        + medida_cota_acima.N_SPT
+        + medida_cota_abaixo.N_SPT
+    ) / 3
 
 
 def calcular_Rp(alfa, Np, K, area_ponta):
@@ -93,7 +47,7 @@ def calcular_Rp(alfa, Np, K, area_ponta):
     return alfa * K * Np * area_ponta
 
 
-def calcular_Nl(perfil_spt, cota_asentamento):
+def calcular_Nl(perfil_spt: PerfilSPT, cota_asentamento: int):
     """
     Calcula o Nl_SPT médio ao longo do fuste da estaca,
     desconsiderando as cotas utilizadas no Np.
@@ -108,14 +62,13 @@ def calcular_Nl(perfil_spt, cota_asentamento):
 
     Nl = 0
     n = 0
+    cota_acima = cota_asentamento - 1
 
-    index_cota_asentamento = cota_asentamento - 1  # Index baseado em 0
-    index_cota_acima = index_cota_asentamento - 1
-    if index_cota_acima <= 0:
+    if cota_acima not in perfil_spt:
         return 0
 
-    for i in range(0, index_cota_acima):
-        Nl += perfil_spt[i][0]
+    for i in range(1, cota_acima):
+        Nl += perfil_spt.obter_medida(i).N_SPT
         n += 1
     return Nl / n if n > 0 else 0
 
@@ -237,9 +190,8 @@ def calcular_decourt_quaresma(
     # Cálculo da Resistência de Ponta (Rp)
 
     cota_asentamento = estaca.comprimento
-    index_cota_asentamento = cota_asentamento - 1
     Np = calcular_Np(perfil_spt, cota_asentamento)
-    tipo_solo_ponta = perfil_spt[index_cota_asentamento][1]
+    tipo_solo_ponta = perfil_spt.obter_medida(cota_asentamento).tipo_solo
     tipo_solo_ponta = normalizar_tipo_solo(tipo_solo_ponta, 'decourt_quaresma')
 
     K = coef_K[tipo_solo_ponta][estaca.processo_construcao]
@@ -251,10 +203,9 @@ def calcular_decourt_quaresma(
     # Cálculo da Resistência Lateral (Rl)
     Rl = 0
     Rl_parcial = 0
-    for i in range(0, cota_asentamento):
-        cota = i + 1
+    for cota in range(1, cota_asentamento + 1):
         Nl = calcular_Nl(perfil_spt, cota)
-        tipo_solo_lateral = perfil_spt[i][1]
+        tipo_solo_lateral = perfil_spt.obter_medida(cota).tipo_solo
         tipo_solo_lateral = normalizar_tipo_solo(
             tipo_solo_lateral, 'decourt_quaresma'
         )
@@ -270,32 +221,39 @@ def calcular_decourt_quaresma(
     }
 
 
-if __name__ == '__main__':
-    from pprint import pprint
+def calcular_capacidade_estaca(
+    perfil_spt: PerfilSPT,
+    tipo_estaca: str,
+    processo_construcao: str,
+    formato: str,
+    secao_transversal: float,
+):
+    """
+    Calcula a capacidade de carga de uma estaca pelo método de
+    Décourt-Quaresma.
 
-    # Exemplo de uso
-    perfil_spt = [
-        (3, 'areia_argilosa'),
-        (3, 'areia_argilosa'),
-        (5, 'areia_argilosa'),
-        (6, 'argila_arenosa'),
-        (8, 'argila_arenosa'),
-        (13, 'argila_arenosa'),
-        (17, 'argila_arenosa'),
-        (25, 'argila_arenosa'),
-        (27, 'argila_areno_siltosa'),
-        (32, 'argila_areno_siltosa'),
-        (36, 'argila_areno_siltosa'),
-    ]
-    resultado = []
+    Args:
+        perfil_spt: Perfil SPT da estaca.
+        tipo_estaca: Tipo de estaca (cravada, escavada, escavada_bentonita,
+            helice_continua, raiz, injetada).
+        processo_construcao: escavada, deslocamento.
+        formato: Formato da estaca (circular, quadrada).
+        secao_transversal: Seção transversal da estaca em metros.
+
+    Returns:
+        dict: Resistências de ponta, lateral e total em kN.
+    """
+
+    resultado: list[dict] = []
     for i in range(1, len(perfil_spt) + 1):
         estaca = Estaca(
-            tipo='cravada',
-            processo_construcao='deslocamento',
-            formato='quadrada',
-            secao_transversal=0.3,
+            tipo=tipo_estaca,
+            processo_construcao=processo_construcao,
+            formato=formato,
+            secao_transversal=secao_transversal,
             comprimento=i,
         )
+
         decourt_quaresma = calcular_decourt_quaresma(
             perfil_spt,
             estaca,
@@ -311,4 +269,36 @@ if __name__ == '__main__':
                 'capacidade_total': decourt_quaresma['capacidade_total'],
             }
         )
+    return resultado
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    # Exemplo de uso
+    perfil_spt = PerfilSPT()
+
+    perfil_spt.adicionar_medidas(
+        [
+            (1, 3, 'areia_argilosa'),
+            (2, 3, 'areia_argilosa'),
+            (3, 5, 'areia_argilosa'),
+            (4, 6, 'argila_arenosa'),
+            (5, 8, 'argila_arenosa'),
+            (6, 13, 'argila_arenosa'),
+            (7, 17, 'argila_arenosa'),
+            (8, 25, 'argila_arenosa'),
+            (9, 27, 'argila_areno_siltosa'),
+            (10, 32, 'argila_areno_siltosa'),
+            (11, 36, 'argila_areno_siltosa'),
+        ]
+    )
+    resultado = calcular_capacidade_estaca(
+        perfil_spt,
+        tipo_estaca='cravada',
+        processo_construcao='deslocamento',
+        formato='quadrada',
+        secao_transversal=0.3,
+    )
+
     pprint(resultado, sort_dicts=False)
