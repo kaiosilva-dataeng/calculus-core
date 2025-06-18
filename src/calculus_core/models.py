a0ecc42214110e889e018338750022065f1b5819
Kaio Silva <98492492+kaiosilva-dataeng@users.noreply.github.com>
2025-06-17 21:48:51 -0300

    feat: implement Estaca and MedidaSPT classes with associated methods for calculations and data handling

---
+++ src/calculus_core/models.py
@@ -0,0 +1,145 @@
+import math
+
+
+class Estaca:
+    def __init__(
+        self,
+        tipo,
+        processo_construcao,
+        formato,
+        secao_transversal,
+        comprimento,
+    ):
+        if formato not in ['circular', 'quadrada']:
+            raise ValueError(
+                'Formato de estaca deve ser "circular" ou "quadrada".'
+            )
+        self.tipo = tipo
+        self.processo_construcao = processo_construcao
+        self.formato = formato
+        self.secao_transversal = secao_transversal
+        self.comprimento = comprimento
+
+    def area_ponta(self):
+        if self.formato == 'circular':
+            raio = self.secao_transversal / 2
+            return math.pi * (raio**2)
+        elif self.formato == 'quadrada':
+            largura = self.secao_transversal
+            return largura**2
+
+    def perimetro(self):
+        if self.formato == 'circular':
+            raio = self.secao_transversal / 2
+            return 2 * math.pi * raio
+        elif self.formato == 'quadrada':
+            largura = self.secao_transversal
+            return 4 * largura
+
+    def area_lateral(self):
+        if self.formato == 'circular':
+            raio = self.secao_transversal / 2
+            return math.pi * raio * self.comprimento
+        elif self.formato == 'quadrada':
+            largura = self.secao_transversal
+            return largura * self.comprimento
+
+
+class MedidaSPT:
+    """
+    Representa uma única medida do ensaio SPT, agora com suporte
+    para registrar condições de impenetrável.
+    """
+
+    def __init__(self, profundidade, N_SPT, tipo_solo):
+        """
+        Args:
+            profundidade (float): Profundidade da medida em metros.
+            N_SPT (int): Número de golpes (soma dos 2 últimos segmentos de
+                15cm).
+            tipo_solo (str): Tipo de solo. Pode ser "impenetravel".
+        """
+        self.profundidade = profundidade
+        self.N_SPT = N_SPT
+        self.tipo_solo = tipo_solo
+
+    def __repr__(self):
+        return (
+            f'MedidaSPT(profundidade={self.profundidade}, '
+            f'N_SPT={self.N_SPT}, tipo_solo={self.tipo_solo})'
+        )
+
+
+class PerfilSPT:
+    def __init__(self, nome_sondagem: str = 'SP-01'):
+        self.nome_sondagem = nome_sondagem
+        self.medidas: list[MedidaSPT] = []
+
+    def adicionar_medida(self, profundidade, N_SPT, tipo_solo):
+        medida = MedidaSPT(profundidade, N_SPT, tipo_solo)
+        self.medidas.append(medida)
+        self.medidas.sort(key=lambda x: x.profundidade)
+
+    def adicionar_medidas(self, dados: list[tuple]):
+        """
+        Adiciona múltiplas medidas ao perfil SPT.
+
+        Args:
+            dados (list[tuple]): Lista de tuplas contendo profundidade, N_SPT e
+                tipo_solo.
+        """
+        for profundidade, N_SPT, tipo_solo in dados:
+            self.adicionar_medida(profundidade, N_SPT, tipo_solo)
+            self.medidas.sort(key=lambda x: x.profundidade)
+
+    def __repr__(self):
+        return f'PerfilSPT(nome_sondagem={self.nome_sondagem}, medidas={self.medidas})'  # noqa: E501
+
+    def __len__(self):
+        return len(self.medidas)
+
+    def __getitem__(self, index):
+        return self.medidas[index]
+
+    def __iter__(self):
+        return iter(self.medidas)
+
+    def __contains__(self, profundidade):
+        """
+        Verifica se uma profundidade específica está registrada no perfil SPT.
+
+        Args:
+            profundidade (int): Profundidade a ser verificada.
+
+        Returns:
+            bool: True se a profundidade estiver registrada,
+                False caso contrário.
+        """
+        return any(
+            medida.profundidade == profundidade for medida in self.medidas
+        )
+
+    def obter_medida(self, profundidade: int, aprox: bool = False):
+        if not self.medidas:
+            raise ValueError('Nenhuma medida registrada no perfil SPT.')
+
+        if profundidade > self.medidas[-1].profundidade:
+            return MedidaSPT(profundidade, 50, 'impenetravel')
+        if profundidade < self.medidas[0].profundidade and not aprox:
+            raise ValueError(
+                (
+                    f'Profundidade {profundidade}m está acima da profundidade '
+                    'mínima registrada.'
+                )
+            )
+        for medida in self.medidas:
+            if medida.profundidade == profundidade:
+                return medida
+        if aprox:
+            return min(
+                self.medidas, key=lambda x: abs(x.profundidade - profundidade)
+            )
+
+        raise ValueError(
+            f'Medida não encontrada para a profundidade {profundidade}m.'
+        )
